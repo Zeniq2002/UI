3d71751381a3e60c6fa05579872260e841d02a06
Dejan Bavdaz <dejan.bavdaz@elev.ga.lbs.se>
2019-01-10 10:57:53 +0100

    UI test
    UI

---
+++ Assets/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public void CoolRotate(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+}
